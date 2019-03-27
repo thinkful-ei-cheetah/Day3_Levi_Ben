25eeee2f5feba703975b4f6bc23e2dad0f85a34a
Ben Tilghman <ben.tilghman@gmail.com>
2019-03-27 18:24:58 -0400

    DONE!

--- object_drills3.js
+++ object_drills3.js
@@ -110,4 +110,43 @@ console.log(decoder(eCode));
 
 // 7. Factory Functions with LOTR ---------------------------------
 
+function createCharacter(name, nickname, race, origin, attack, defense) {
+    return {
+        name,
+        nickname,
+        race,
+        origin,
+        attack,
+        defense,
+        describe: function () {
+            console.log(
+                `${this.name} is a ${this.race} from ${this.origin}`);
+        },
+        evaluateFight: function (character) {
+            let yourAttack = character.defense > this.attack ? 0 : this.attack - character.defense;
+            let yourDamage = this.defense > character.attack ? 0 : character.attack - this.defense;
+            return `Your opponent takes ${yourAttack} damage and you receive ${yourDamage} damage.`;
+        },
+        weaponEquip: function (weapon) {
+            this.describe = function () {
+                console.log(
+                    `${this.name} is a ${this.race} from ${this.origin} who uses a ${weapon}`);
+            }
+        }
+    };
+}
+
+const characters = [
+    createCharacter("Gandalf the White", "gandalf", "Wizard", "Middle Earth", 10, 6),
+    createCharacter("Bilbo Baggins", "bilbo", "Hobbit", "The Shire", 2, 1),
+    createCharacter("Frodo Baggins", "frodo", "Hobbit", "The Shire", 3, 2),
+    createCharacter("Aragorn Son of Arathorn", "aragorn", "Man", "Dunnedain", 6, 8),
+    createCharacter("Legolas", "legolas", "Elf", "Woodland Realm", 8, 5),
+    createCharacter("Arwen Undomiel", "arwyl", "Half-Elf", "Rivendell", 5, 6),
+];
+
+const findAragorn = characters.find(object => object.nickname === "aragorn").describe();
+const filterHobbits = characters.filter(object => object.race === "Hobbit");
+const filterAttack = characters.filter(object => object.attack > 5);
+
 // 8. BONUS: A Database Search ------------------------------------
