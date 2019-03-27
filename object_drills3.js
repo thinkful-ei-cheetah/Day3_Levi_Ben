c909ee61bd907bf080f9968579ce4340430a699c
Levi Paulk <levipaulk@gmail.com>
2019-03-27 15:26:27 -0400

    added sections to obj drills 3

--- object_drills3.js
+++ object_drills3.js
@@ -1,1 +1,16 @@
 'use strict';
+// 1. Object initializers and methods------------------------------
+
+// 2. Iterating over an object's properties -----------------------
+
+// 3. Arrays in objects -------------------------------------------
+
+// 4. Arrays of objects -------------------------------------------
+
+// 5. Properties that aren't there --------------------------------
+
+// 6. Cracking the code -------------------------------------------
+
+// 7. Factory Functions with LOTR ---------------------------------
+
+// 8. BONUS: A Database Search ------------------------------------
