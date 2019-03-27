0db52265189fc897c5308b814b26a80944312cc7
Ben Tilghman <ben.tilghman@gmail.com>
2019-03-27 12:50:01 -0400

    Drills 1 and 2

--- object_drills1.js
+++ object_drills1.js
@@ -53,80 +53,125 @@ function createMyObject() {
 })();
 // Object Updater ---------------------------------------------------------
 function updateObject (obj) {
-	obj.foo = 'foo';
-	obj.bar = 'bar';
-	obj.bizz = 'bizz';
-	obj.bang = 'bang';
-	return obj;
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+    return obj;
 }
 //Object Updater TEST -----------------------------------------------------
 (function testUpdateObject() {
-	var oldObj = {
+    var oldObj = {
 	  cats: 'cats',
 	  dogs: 'dogs',
-	};
-	var newObj = updateObject(oldObj);
-	if (typeof newObj !== 'object') { 
+    };
+    var newObj = updateObject(oldObj);
+    if (typeof newObj !== 'object') { 
 	  console.error('ERROR: `createMyObject` must return an object');
-	  return false
-	}
-	['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+	  return false;
+    }
+    ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
 	  if (!(key in newObj)) {
-		console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
-		return false;
+            console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
+            return false;
 	  }
-	});
-	['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+    });
+    ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
 	  if (newObj[key] !== key) {
-		console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
-		return false;
+            console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
+            return false;
 	  }
-	});
-	if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
+    });
+    if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
 	  console.error('ERROR: your function doesn\'t preserve existing key/value pairs');
 	  return false;
-	}
-	console.log('SUCCESS: `updateObject` works correctly!');
+    }
+    console.log('SUCCESS: `updateObject` works correctly!');
 	
-  })();
+})();
 //Self-reference ----------------------------------------------------------
 function personMaker () {
-	const person = {
-		firstName: 'Paul',
-		lastName: 'Jones',
-		fullName: function () {
-			return `${this.firstName} ${this.lastName}`;
-		},
-	};
-	return person;
+    const person = {
+        firstName: 'Paul',
+        lastName: 'Jones',
+        fullName: function () {
+            return `${this.firstName} ${this.lastName}`;
+        },
+    };
+    return person;
 }
 //Self-reference TEST -----------------------------------------------------
 (function testPersonMaker() {
-	var person = personMaker();
-	if (typeof person !== 'object') {
-		console.error('ERROR: `personMaker` must return an object');
-		return false;
-	}
-	if (typeof person.fullName !== 'function') {
-		console.error('ERROR: `fullName` must be a method');
-		return false;
-	}
-	if (person.fullName() !== 'Paul Jones') {
-		console.error(
-		'ERROR: The value for `fullName` should be "Paul Jones" but was ' +
+    var person = personMaker();
+    if (typeof person !== 'object') {
+        console.error('ERROR: `personMaker` must return an object');
+        return false;
+    }
+    if (typeof person.fullName !== 'function') {
+        console.error('ERROR: `fullName` must be a method');
+        return false;
+    }
+    if (person.fullName() !== 'Paul Jones') {
+        console.error(
+            'ERROR: The value for `fullName` should be "Paul Jones" but was ' +
 			person.fullName()
-		);
-		return false;
-	}
-	person.firstName = 'Lisa';
-	person.lastName = 'Simpson';
-	if (person.fullName() !== 'Lisa Simpson') {
-		console.error(
-		'`personMaker` is not using self reference correctly. ' +
+        );
+        return false;
+    }
+    person.firstName = 'Lisa';
+    person.lastName = 'Simpson';
+    if (person.fullName() !== 'Lisa Simpson') {
+        console.error(
+            '`personMaker` is not using self reference correctly. ' +
 			'When firstName set to "Lisa" and lastName set to "Simpson", ' +
 			'should return "Lisa Simpson" but returned ' +
 			person.fullName()
-		);
-	}
-	console.log('SUCCESS: `updateObject` works correctly!');
-  })();
+        );
+    }
+    console.log('SUCCESS: `updateObject` works correctly!');
+})();
+
+
+//keyDeleter problem---------------------------------------------------
+
+const sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+};
+
+function keyDeleter(obj) {
+    delete obj.foo;
+    delete obj.bar;
+    return obj;
+}
+
+// testKeyDeleter Test--------------------------------------------
+
+(function testKeyDeleter() {
+    var obj = keyDeleter({
+        foo: 'foo',
+        bar: 'bar',
+        bizz: 'bizz',
+        bang: 'bang',
+    });
+
+    if (typeof obj !== 'object') {
+        console.error('ERROR: `keyDeleter` must be return an object');
+        return false;
+    }
+    ['foo', 'bar'].forEach(function (key) {
+        if (key in obj) {
+            console.error('`keyDeleter` did not delete the key for ' + key);
+            return false;
+        }
+    });
+    ['bizz', 'bang'].forEach(function (key) {
+        if (!(key in obj && obj[key] === key)) {
+            console.error('`keyDeleter` is deleting keys other than `foo` and `bar`');
+            return false;
+        }
+    });
+    console.log('SUCCESS: `keyDeleter` works correctly!');
+})();
---
+++ object_drills2.js
@@ -0,0 +1,125 @@
+'use strict';
+
+//Make Student Reports-----------------------------------------
+function makeStudentsReport(data) {
+    return data.map(obj => `${obj.name}: ${obj.grade}`);    
+}
+
+//Make Student Reports Test-----------------------------------
+
+function testIt() {
+    const testData = [
+        { name: 'Jane Doe', grade: 'A' },
+        { name: 'John Dough', grade: 'B' },
+        { name: 'Jill Do', grade: 'A' },
+    ];
+
+    const expectations = ['Jane Doe: A', 'John Dough: B', 'Jill Do: A'];
+
+    const results = makeStudentsReport(testData);
+
+    if (!(results && results instanceof Array)) {
+        console.error('FAILURE: `makeStudentsReport` must return an array');
+        return;
+    }
+    if (results.length !== testData.length) {
+        console.error(
+            'FAILURE: test data had length of ' +
+            testData.length +
+            ' but `makeStudentsReport` returned array of length ' +
+            results.length
+        );
+        return;
+    }
+    for (let i = 0; i < expectations.length; i++) {
+        let expect = expectations[i];
+        if (
+            !results.find(function (item) {
+                return item === expect;
+            })
+        ) {
+            console.error(
+                'FAILURE: `makeStudentsReport` is not ' + 'producing expected strings'
+            );
+            return;
+        }
+    }
+    console.log('SUCCESS: `makeStudentsReport` is working');
+}
+
+testIt();
+
+//Enroll in summer school-----------------------------------------
+
+const studentData = [
+    {
+        name: 'Tim',
+        status: 'Current student',
+        course: 'Biology',
+    },
+    {
+        name: 'Sue',
+        status: 'Withdrawn',
+        course: 'Mathematics',
+    },
+    {
+        name: 'Liz',
+        status: 'On leave',
+        course: 'Computer science',
+    },
+];
+
+function enrollInSummerSchool(students) {
+    return students.map(obj => Object.assign( obj , { status: "In Summer school" }));
+}
+
+//Enroll in summer school tests-----------------------------------------
+
+function testIt() {
+    var testData = [
+        {
+            name: 'Burt',
+            status: 'Playing hooky',
+            course: 'Biology',
+        },
+        {
+            name: 'Melanie',
+            status: 'Sick',
+            course: 'Mathematics',
+        },
+        {
+            name: 'Leonard',
+            status: 'AWOL',
+            course: 'Computer science',
+        },
+    ];
+
+    var results = enrollInSummerSchool(testData);
+
+    if (!(results && results instanceof Array)) {
+        console.error('FAILURE: `enrollSummerSchool` must return an array');
+        return;
+    }
+    var result = testData.every(function (student) {
+        var match = results.find(function (_student) {
+            return (
+                _student.name === student.name &&
+                _student.course === student.course &&
+                _student.status.toLowerCase() === 'in summer school'
+            );
+        });
+        return match !== undefined;
+    });
+    if (!result) {
+        console.error(
+            'FAILURE: `enrollSummerSchool` should return ' +
+            'original key/value pairs for each student, and ' +
+            'update `status` to "In Summer school": ' +
+            JSON.stringify(results)
+        );
+    } else {
+        console.info('SUCCESS: `enrollSummerSchool` is working');
+    }
+}
+
+testIt();
