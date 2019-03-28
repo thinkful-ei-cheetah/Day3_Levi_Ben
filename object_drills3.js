25a18b21b840ed6429d5c1d1006c419eb0eea214
Levi Paulk <levipaulk@gmail.com>
2019-03-28 10:20:09 -0400

    add Rich's solution to bonus drill in drills3

--- object_drills3.js
+++ object_drills3.js
@@ -149,3 +149,43 @@ characters[0].weaponEquip('Machine Gun');
 characters[0].describe();
 
 // 8. BONUS: A Database Search ------------------------------------
+
+const Database = {
+    store: {
+        heroes: [
+            { id: 1, name: 'Captain America', squad: 'Avengers' },
+            { id: 2, name: 'Iron Man', squad: 'Avengers' },
+            { id: 3, name: 'Spiderman', squad: 'Avengers' },
+            { id: 4, name: 'Superman', squad: 'Justice League' },
+            { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+            { id: 6, name: 'Aquaman', squad: 'Justice League' },
+            { id: 7, name: 'Hulk', squad: 'Avengers' },
+          ]
+    },
+}
+
+findOne: function(query) {
+    const queryKeys = Object.keys(query);
+    if (queryKeys.length === 0) return null;
+    // loop through all heros
+    // for each  hero, check query key/value against hero key/value
+    const foundHero = this.store.heroes.find(hero => {
+        for ( const key of queryKeys) {
+            if ( query[key] !== hero[key]) return false;
+        }
+        return true;
+    });
+    // if anything doesn't match, immediately break to next hero
+    // if all key/value match, immediately return current hero
+    // if reach end, return null
+    return foundHero || null;
+};
+
+Database.findOne();
+
+function testDatabase() {
+    const test1 = Database.findOne({ id: 1});
+    console.assert( test1 !== null && test1.id === 1 );
+}
+
+testDatabase();
